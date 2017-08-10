3751da5d2caece32dbdb4b078f55ceed0e2bbfd4
Carlos Coello <carlos.coello05@outlook.com>
2017-08-10 14:04:43 -0500

    changes

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  uri: 'mongodb://carlos:cabclos@ds053708.mlab.com:53708/heroku_86n7f9h2',
+  uri: 'mongodb://carlos:cabclos@ds021701.mlab.com:21701/heroku_5q4w10bv',
   db:'mean-stack-todo'
 }
